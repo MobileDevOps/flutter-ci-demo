69367d07e9951bc9a4583fd956133fc7043ab304
messeb <sebastian@messeb.com>
2019-09-02 17:02:40 +0200

    Add initial Flutter app

---
+++ android/app/src/main/java/net/mobiledevops/fluttercidemo/MainActivity.java
@@ -0,0 +1,13 @@
+package net.mobiledevops.fluttercidemo;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
